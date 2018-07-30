7f2481d57404e9eb63264fb13613a214a2261216
Carlos Garcia <cgarciapieto@gmail.com>
2018-07-30 15:20:34 -0500

    javascript-day1-if-cgarciapieto.iml

---
+++ excercise8.js
@@ -0,0 +1,11 @@
+var inputNumber = parseInt(prompt("Enter a number"));
+
+if(inputNumber > 0)
+{
+    alert("POSITIVE")
+}
+if (inputNumber < 0)
+{
+    alert("NEGATIVE!")
+}
+
---
+++ excercise9.js
@@ -0,0 +1,16 @@
+var inputNumber1 = parseInt(prompt("Enter a number"));
+if (inputNumber1 == 1)
+{
+    alert('correct')
+}
+else if (inputNumber1 == 2)
+{
+    alert("correct")
+}
+else if (inputNumber1 == 3)
+{
+    alert("correct")
+}
+else {
+    ERROR
+}
